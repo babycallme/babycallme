e8d01dc4593ee61f86a3725ea78b110633178a59
shauryarjain <shauryarjain@gmail.com>
2014-09-14 00:46:28 -0400

    Made it work
    listening FTW

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var logger = require('morgan');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,7 +11,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 app.set('port', process.env.PORT || 3000);
 
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({secret: 'codeweekend'}));
@@ -20,3 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var routes = require('./routes');
 app.use('/', routes);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
